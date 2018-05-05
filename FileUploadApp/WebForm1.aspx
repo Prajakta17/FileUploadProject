1b599f119c111ad8aeec55a673e803789bacfb37
Prajakta17 <prajaktakumar17@gmail.com>
2018-05-06 00:54:21 +0530

    adding files

---
+++ FileUploadApp/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FileUploadApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:FileUpload ID="FileUpload1" runat="server" style="margin-left: 360px" />
+        <p style="margin-left: 440px">
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload" 
+                Width="70px" />
+        </p>
+
+    </form>
+</body>
+</html>
---
+++ FileUploadApp/WebForm1.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.IO;
+
+namespace FileUploadApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string MyConnectionString = "Server=localhost;Database=files_data;uid=root";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            FileInfo fileInfo = new FileInfo(FileUpload1.FileName);
+            byte[] FileContent = FileUpload1.FileBytes;
+            bool exist = false;
+            string name = fileInfo.Name;
+            MySqlConnection connection = new MySqlConnection(MyConnectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd1 = connection.CreateCommand();
+                cmd1.CommandText = "SELECT * from files_data where file_name = @name AND physical_file = @physicalFile";
+                cmd1.Parameters.AddWithValue("@name", name);
+                cmd1.Parameters.AddWithValue("@physicalFile", FileContent);
+                MySqlDataReader dr = cmd1.ExecuteReader();
+                while (dr.Read())
+                {
+                        if (dr.HasRows == true)
+                        {
+                            exist = true;
+                            Response.Write("<script>alert('File already exists');</script>");
+                        }
+                 }
+                dr.Close();
+                
+                if (exist == false)
+                {
+                    MySqlCommand cmd2 = connection.CreateCommand();
+                    cmd2.CommandText = "SELECT * from files_data where file_name = @name2";
+                    cmd2.Parameters.AddWithValue("@name2", name);
+                    MySqlDataReader dr2 = cmd2.ExecuteReader();
+                    while (dr2.Read())
+                    {
+                        if (dr2.HasRows == true)
+                        {
+                            exist = true;
+                            Response.Write("<script>alert('A file with similar name already exists! Please rename the file');</script>");
+                        }
+                    }
+                    dr2.Close();
+                }
+                if (exist == false)
+                {
+                    MySqlCommand cmd3 = connection.CreateCommand();
+                    cmd3.CommandText = "SELECT * from files_data where physical_file = @physicalFile3";
+                    cmd3.Parameters.AddWithValue("@physicalFile3", FileContent);
+                    MySqlDataReader dr3 = cmd3.ExecuteReader();
+                    while (dr3.Read())
+                    {
+                        if (dr3.HasRows == true)
+                        { 
+                            exist = true;
+                            Response.Write("<script>alert('File already exists with a different name. Do you want to overwrite it?');</script>");
+                        }
+                    }
+                    dr3.Close();
+                }
+                if (exist==false)
+                {
+                    //connection.Open();
+                    MySqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = "INSERT INTO files_data(file_name,physical_file)VALUES(@name,@Content)";
+                    cmd.Parameters.AddWithValue("@name", name);
+                    //cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.Add("@Content", MySqlDbType.VarBinary).Value = FileContent;
+                    cmd.ExecuteNonQuery();
+                    Response.Write("<script>alert('File is uploaded successfully');</script>");
+                }
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    
+                    connection.Close();
+
+                    // LoadData();
+                }
+            }
+        }
+    }
+}
